e0dbf0547f6c6e71593453b1b371e8dfa7811cb0
josh <joshcarr3007@gmail.com>
2023-08-19 11:04:25 +0100

    add path to admin

--- sub4/urls.py
+++ sub4/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from django.views import generic
 from .views import *
 
 urlpatterns = [
+    path('admin/', admin.site.urls), 
     path("", viewSub4, name="sub4"),
     ]
